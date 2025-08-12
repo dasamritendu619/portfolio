5e877b042a9bc1a5ed6312742d9ba2ce52dbc140
Amritendu Das <cse21114@cemk.ac.in>
2025-08-12 11:11:43 +0530

    fix: update axios version and improve education content details

--- src/components/shared/Education.tsx
+++ src/components/shared/Education.tsx
@@ -5,16 +5,16 @@ import { Timeline } from "@/components/ui/timeline";
 export default function Education() {
   const data = [
     {
-      title: "B.Tech in CS",
+      title: "B.Tech in CSE",
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm xl:text-lg font-normal mb-8">
-            I am currently pursuing  in my B.Tech in Computer Science & Engineering from College of Engineering & Management, Kolaghat under MAKAUT, WB. 
+            I have completed my B.Tech Degree in Computer Science & Engineering from College of Engineering & Management, Kolaghat under MAKAUT, WB in 2025.
             <br />
-            I got 8.16 CGPA upto 7th semester in 2025.
+            I got 8.19 CGPA in my B.Tech Degree.
           </p>
           <div className="grid grid-cols-2 gap-4">
-            
+
           </div>
         </div>
       ),
@@ -24,12 +24,12 @@ export default function Education() {
       content: (
         <div>
           <p className="text-neutral-800 dark:text-neutral-200 text-xs xl:text-lg  md:text-sm font-normal mb-8">
-            I have completed my Higher Secondary from Tamluk Hamilton High School in 2021 under WBCHSE. 
+            I have completed my Higher Secondary from Tamluk Hamilton High School in 2021 under WBCHSE.
             <br />
             I got 88.6% in my Higher Secondary.
           </p>
           <div className="grid grid-cols-2 gap-4">
-            
+
           </div>
         </div>
       ),
@@ -44,12 +44,12 @@ export default function Education() {
             I got 88.46% in my Secondary.
           </p>
           <div className="grid grid-cols-2 gap-4">
-           
+
           </div>
         </div>
       ),
     },
-    
+
   ];
   return (
     <div className="w-full" id="education">
